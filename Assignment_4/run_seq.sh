006ab10c784c98e3169de98c2896369fc8ceb585
Kristjan Kostanjšek <kristjan.kostanjsek@gmail.com>
2025-05-24 15:40:12 +0200

    Basic tasks done (sequential and row-wise MPI)

---
+++ Assignment_4/run_seq.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH --reservation=fri
+#SBATCH --job-name=hpc_ass_3
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --output=out/grayscott_seq.log
+#SBATCH --time=01:30:00
+
+gcc -O2 -lm -fopenmp grayscott_seq.c -o grayscott_seq
+
+srun ./grayscott_seq 256 5000 1 0.16 0.08 0.060 0.062
